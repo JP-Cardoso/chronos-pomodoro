22f8b74e73943e8c72d7a11b3d32d3c1136dbb6d
jp-cardoso <jhonpoal@hotmail.com>
2025-04-06 16:45:37 -0300

    feat: Create input component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Conatiner from './components/Container';
 import Logo from './components/Logo';
 import Menu from './components/Menu';
 import CountDown from './components/CountDown';
+import DefaultInput from './components/DefaultInput';
 
 export default function App() {
 
@@ -23,6 +24,34 @@ export default function App() {
 
       <Conatiner>
         <CountDown />
+      </Conatiner>
+
+      <Conatiner>
+        <form action="" className="form">
+          <div className="formRow">
+            <DefaultInput
+              id='meuInput'
+              type='text'
+              labelText='label'
+              placeholder='Digite algo'
+              defaultValue='Valor preenchido'
+              disabled
+            />
+          </div>
+
+          <div className="formRow">
+            <p>
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Conatiner>
     </>
   );
