397e1537355cc048bbebc8e8bcc7573ef5508848
jp-cardoso <jhonpoal@hotmail.com>
2025-04-12 15:08:58 -0300

    Feat: Create About and Not Found pages

---
+++ src/components/GenericHtml/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import style from "./style.module.css";
+
+type GenericHtmlProps = {
+  children: React.ReactNode;
+}
+
+export default function GenericHtml({
+  children
+}: GenericHtmlProps) {
+
+  return (
+    <div className={style.genericHtml}>
+      {children}
+    </div>
+  )
+}
