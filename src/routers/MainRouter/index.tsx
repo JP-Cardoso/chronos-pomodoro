a1ab24cef3c911c2d5818486211dd243a0b4d292
jp-cardoso <jhonpoal@hotmail.com>
2025-04-28 23:05:06 -0300

    feat: Create mainRouter

---
+++ src/routers/MainRouter/index.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, useLocation } from "react-router";
+import AboutPomodoro from "../../pages/AboutPomodoro";
+import HomePage from "../../pages/Home";
+import NotFound from "../../pages/NotFound";
+import { useEffect } from "react";
+
+function ScrollTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
+}
+
+export default function MainRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <ScrollTop />
+    </BrowserRouter>
+  );
+}
